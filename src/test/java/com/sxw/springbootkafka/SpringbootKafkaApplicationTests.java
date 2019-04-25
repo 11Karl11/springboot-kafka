5963f391eaf3f742537fa3c11109d3b06e4eb42e
suxiongwei <suxiongwei@kaoshixing.com>
2019-04-25 19:08:10 +0800

    Spring Boot2.0 整合 Kafka

---
+++ src/test/java/com/sxw/springbootkafka/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,36 @@
+package com.sxw.springbootkafka;
+
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.UUID;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootKafkaApplicationTests {
+
+	@Autowired
+	private KafkaSender<User> kafkaSender;
+
+	@Test
+	public void kafkaSend() throws InterruptedException {
+		//模拟发消息
+		for (int i = 0; i < 5; i++) {
+
+			User user = new User();
+			user.setId(System.currentTimeMillis());
+			user.setMsg(UUID.randomUUID().toString());
+			user.setSendTime(new Date());
+
+			kafkaSender.send(user);
+			Thread.sleep(3000);
+
+		}
+	}
+
+}
